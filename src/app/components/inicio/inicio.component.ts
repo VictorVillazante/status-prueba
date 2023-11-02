4f003f553744361750de97de421e2362b8f28478
VictorVillazante <villazante.bernabe.victor@gmail.com>
2023-11-02 11:49:02 -0400

    fix:fix responsive on navbar

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+  readonly caracteristicas_status=[
+    {
+      "img_caracteristica":"../../../assets/images/img_inicio/icon_perfiles_profesionales.png",
+      "titulo":"Perfiles profesionales",
+      "descripcion":"Encuentra a los mejores profesionales del derecho que te brindarán asesoramiento."
+    },
+    {
+      "img_caracteristica":"../../../assets/images/img_inicio/icon_revista_juridica.png",
+      "titulo":"Perfiles profesionales",
+      "descripcion":"Encuentra a los mejores profesionales del derecho que te brindarán asesoramiento."
+    }
+  ]
 }
