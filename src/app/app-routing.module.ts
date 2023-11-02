50c4733f4e127a764c2b4a999f5cac30863567cc
VictorVillazante <villazante.bernabe.victor@gmail.com>
2023-11-02 09:45:42 -0400

    add:add bootstrap to proyect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: 'home', component:  InicioComponent},
+  { path: 'not-found', component:  InicioComponent},
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
